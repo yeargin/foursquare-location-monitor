70ebff133938d8807b1c30e30d8584a7bb61ff9a
Stephen Yeargin <stephen.yeargin@gmail.com>
2012-03-04 14:16:51 -0600

    Making MediaTemple happy. usr/bin/php5

--- application/cron/foursquare_checks.sh
+++ application/cron/foursquare_checks.sh
@@ -1,12 +1,12 @@
 echo ""
 echo "==== Executing Daily Check Script ==="
 echo ""
-/usr/bin/php index.php checks check_venues daily
+/usr/bin/php5 index.php checks check_venues daily
 
 echo ""
 echo "==== Executing Live Check Script ==="
 echo ""
-/usr/bin/php index.php checks check_venues live
+/usr/bin/php5 index.php checks check_venues live
 
 echo ""
 echo "Scripts complete!"

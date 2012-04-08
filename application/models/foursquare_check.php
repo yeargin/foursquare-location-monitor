9abeafa959919494dedd96a52558f7df5b76c28f
Stephen Yeargin <stephen.yeargin@gmail.com>
2012-04-08 14:11:07 -0500

    Fix warning if no tags have been set.

--- application/models/foursquare_check.php
+++ application/models/foursquare_check.php
@@ -207,6 +207,7 @@ class Foursquare_check extends CI_Model {
 		$result = $query->result();
 		
 		// Build indexed list of tags
+		$output = array();
 		if (is_array($result)):
 			foreach ($result as $row):
 				$output[$row->check_id][] = $row->tag;

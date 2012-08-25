e1ff1e3e1f6fd092dc335c0495948fd916be567a
Stephen Yeargin <stephen.yeargin@gmail.com>
2012-08-24 21:08:45 -0500

    Fixing unused jquery.tagit

--- application/views/checks/_date_range_form.php
+++ application/views/checks/_date_range_form.php
@@ -1,12 +1,20 @@
 <h3>Date Range</h3>
 <form>
-<fieldset>
-	<div class="input-prepend">
-		<span class="add-on"><i class="icon-calendar"></i></span><input type="text" value="<?php echo date('n/j/Y', strtotime($date_range['start_date'])); ?>" name="start_date" class="span2 datepicker" />
+<fieldset class="form form-stacked">
+	<div class="control-group">
+		<div class="controls">
+			<div class="input-prepend">
+				<span class="add-on"><i class="icon-calendar"></i></span><input type="text" value="<?php echo date('n/j/Y', strtotime($date_range['start_date'])); ?>" name="start_date" class="span2 datepicker" />
+			</div>
+		</div>
 	</div>
-	<div class="input-prepend">
-		<span class="add-on"><i class="icon-calendar"></i></span><input type="text" value="<?php echo date('n/j/Y', strtotime($date_range['end_date'])); ?>" name="end_date" class="span2 datepicker" />
-	</div>
+	<div class="control-group">	
+		<div class="controls">
+			<div class="input-prepend">
+				<span class="add-on"><i class="icon-calendar"></i></span><input type="text" value="<?php echo date('n/j/Y', strtotime($date_range['end_date'])); ?>" name="end_date" class="span2 datepicker" />
+			</div>
+		</div>
+		</div>
 	<div>
 		<input type="submit" class="btn" value="Change"/>
 	</div>
